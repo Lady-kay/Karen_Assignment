d19ba0bc17099da953d775a34f20d62bdf003ea3
Lady-kay <karenodas@gmail.com>
2025-02-27 18:23:52 +0100

    Initial commit - added index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+//console.log("Assignment")
+let num1 = 15
+let num2 = 7
+
+let addition = num1 + num2
+
+console.log(`Numbers: num1 = ${num1}, num2 = ${num2}`);
+console.log(`Addition: ${num1} + ${num2} = ${addition}`);
+
+//Subtraction
+let subtraction = num1 - num2;
+console.log(`Subtraction: ${num1} - ${num2} = ${subtraction}`);
+
+//Multiplication
+let multiplication = num1 * num2;
+console.log(`Multiplication: ${num1} * ${num2} = ${multiplication}`);
+
+//Division
+let division = num1 / num2;
+console.log(`Division: ${num1} / ${num2} = ${division}`);
+
+//Modulus
+let modulus = num1 % num2;
+console.log(`Modulus: ${num1} % ${num2} = ${modulus}`);
+
+
+
+
+
+
